87701141ff51cb927dfa5ce3c71ad5ba5fe13f22
Monica Wojciechowski <monica.wojciechowski@gmail.com>
2018-09-20 12:55:51 +0200

    changed dir name in webpack files for react router access

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -9,8 +9,8 @@ module.exports = {
   ],
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-    // publicPath: '/static/'
+    filename: 'bundle.js',
+    publicPath: '/static/'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
