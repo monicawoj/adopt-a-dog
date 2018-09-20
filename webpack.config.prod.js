9db7b3badbfc39222fc7db3ac1d1d5a23cbac80a
Monica Wojciechowski <monica.wojciechowski@gmail.com>
2018-09-20 13:00:29 +0200

    relative path change

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,7 +7,7 @@ module.exports = {
     './client/index'
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'dist','static'),
     filename: 'bundle.js',
     publicPath: '/static/'
   },
